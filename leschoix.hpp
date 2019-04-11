d7c15dbac2abb9ff950118ed600525cbd8dc2d19
ac1dloop <rakivvode@gmail.com>
2019-04-11 16:37:10 +0300

    removed unnecessary file and added documentation

--- leschoix.hpp
+++ leschoix.hpp
@@ -1,21 +1,39 @@
-#ifndef LESCHOIX_HPP
-#define LESCHOIX_HPP
+#ifndef LESCHOIX_V2_HPP
+#define LESCHOIX_V2_HPP
 
 #include <string>
 #include <vector>
-#include <functional>
 #include <map>
-#include <iostream>
+#include <algorithm>
 
 namespace Y {
+
 using namespace std;
+
+/**
+ * @struct The OptArg struct
+ *
+ * OptArg stands for options argument
+ * This structure holds exactly one argument value
+ */
 
 struct OptArg {
 
+    /**
+     * @brief OptArg() constructor
+     * @param string value to keep
+     */
     OptArg(const string& str):opt(str){}
 
-    string opt="0";
+    /**
+     * @brief actual value
+     */
+    string opt;
 
+    /**
+     * @brief get value converted to T
+     * @return T - any of arithmetic types
+     */
     template <typename T>
     T get(){
         if (is_same<T, uint8_t>::value||is_same<T, uint16_t>::value||is_same<T, uint32_t>::value)
@@ -39,10 +57,62 @@ struct OptArg {
     }
 };
 
+/**
+ * @brief The lesopt struct
+ *
+ *
+ */
+
 struct lesopt {
 
-    string getOpt(){ return name; }
+    /**
+     * @brief Constructor
+     * @param opt - option that we parse ( -p )
+     * @param alias - full oprtion name ( --port )
+     */
+    lesopt(char opt=0, string alias=""):opt(opt), alias(alias){
+        if (opt&&!::isalnum(opt))
+            throw std::logic_error("option must be a valid ascii characted from latin alphabet");
+    }
+
+    /**
+     * @brief Constructor with default value
+     * @param opt - option that we parse ( -p )
+     * @param alias - full oprtion name ( --port )
+     * @param default_value - value that will be used if nothing is parsed
+     */
+    lesopt(char opt=0, string alias="", string default_value=""):opt(opt), alias(alias), default_value(default_value){
+        if (opt&&!::isalnum(opt))
+            throw std::logic_error("option must be a valid ascii characted from latin alphabet");
+    }
+
+    /**
+     * @brief getOpt
+     * @return option name
+     */
+    string getOpt(){
+        if (opt==0)
+            return alias;
+        else return string(1, opt);
+    }
+
+    /**
+     * @brief getArg
+     * @return first argument from array
+     */
     string getArg(){ return args.at(0).opt; }
+
+    /**
+     * @brief getArg
+     * @return first argument from array converted to T
+     */
+    template<typename T>
+    T getArg(){ return args.at(0).get<T>(); }
+
+    /**
+     * @brief strArgs
+     * @return array of arguments
+     */
     vector<string> strArgs(){
         vector<string> res;
 
@@ -52,19 +122,64 @@ struct lesopt {
         return res;
     }
 
-    string name;
-    vector<OptArg> args;
+    /**
+     * @brief getArgs
+     * @return array<T> of arguments. each converted to T
+     */
+    template<typename T>
+    vector<T> getArgs(){
+        vector<T> res;
+
+        for (auto x: args)
+            res.push_back(x.get<T>());
+
+        return res;
+    }
+
+    char opt; ///< option name ( -p )
+    string alias; ///< full option name ( --port-name )
+    vector<OptArg> args; ///< array of arguments
+    string default_value; ///< default_value that is used in case no args provided
 };
+
+/**
+ * @brief Entry point to parse.
+ * This class should be instantiated with argc and argv
+ * When instantiated it parser input
+ *
+ * Here goes long description
+ */
 
 struct LesChoix {
 
+    /**
+     * @brief LesChoix constructor
+     * @param argc count of arguments
+     * @param argv values of arguments
+     *
+     * Parses argv to array of lesopts.
+     * If any of them empty - applies default value
+     */
     LesChoix(int argc, char **argv){
         Parse(argc, argv);
+
+        cout << "LesChoix()" << endl;
+
+        for (lesopt& it: values){
+            if (it.args.empty())
+                it.args.push_back(it.default_value);
+        }
     }
 
+    /**
+     * @brief Parse
+     * @param argc count of arguments
+     * @param argv values of arguments
+     */
     void Parse(int argc, char **argv){
         string line;
         string opt;
+        vector<lesopt>::iterator it=values.end();
         for (int i=1;i<argc;++i){
             line=argv[i];
 
@@ -77,50 +192,64 @@ struct LesChoix {
                 j=line.find('=');
                 opt=line;
 
-                auto it=special_handlers.find(opt);
-                if (it!=special_handlers.end()){
-                    it->second();
-                    continue;
-                }
-
                 if (j!=string::npos){
                     opt=line.substr(0, j);
+                    line=line.substr(j+1, line.size()-1);
+                } else {
+                    line.clear();
+                }
 
-                    vec.push_back({opt, {line.substr(j+1, line.size()-1)} });
+                if (opt.size()==1){
+                    it=find_if(values.begin(), values.end(), [&opt](const lesopt& x){
+                        return x.opt==opt.at(0);
+                    });
+                } else {
+                    it=find_if(values.begin(), values.end(), [&opt](const lesopt& x){
+                        return x.alias==opt;
+                    });
+                }
 
-                } else vec.push_back({line, {}});
+                if (it!=values.end()&&!line.empty())
+                    (*it).args.push_back(line);
 
             } else {
-                vec.back().args.push_back(line);
+                if (it!=values.end())
+                    (*it).args.push_back(line);
             }
         }
     }
 
-    OptArg& operator[](const string& name){
-        for (size_t i=0;i<vec.size();++i){
-            if (vec[i].name==name)
-                return vec[i].args.at(0);
-        }
+    /**
+     * @brief fast access to lesopt
+     * @param str name of option
+     */
+    auto operator[](const string& str){
+        auto it=find_if(values.begin(), values.end(), [&str](const lesopt& x){
+            return !x.alias.empty()&&x.alias==str;
+        });
 
-        throw std::logic_error("No such opt");
+        if (it!=values.end())
+            return *it;
+        else throw std::logic_error("no such option");
     }
 
-    void process(function<void(string, vector<string>)> handler){
-        for (auto x: vec){
-            handler(x.name, x.strArgs());
-        }
+    /**
+     * @brief overload for short option name
+     * @param str name of option
+     */
+    auto operator[](const char& c){
+        auto it=find_if(values.begin(), values.end(), [&c](const lesopt& x){
+            return x.opt&&x.opt==c;
+        });
+
+        if (it!=values.end())
+            return *it;
+        else throw std::logic_error("no such option");
     }
 
-    void process(function<void(string, vector<OptArg>)> handler){
-        for (auto x: vec)
-            handler(x.name, x.args);
-    }
-
-private:
-    static map<string, function<void()>> special_handlers;
-    vector<lesopt> vec;
+    static vector<lesopt> values; ///< array that holds parsed values
 };
 
-}//Y namespace
+}//YY testing namespace
 
-#endif // LESCHOIX_HPP
+#endif // LESCHOIX_V2_HPP
--- main.cpp
+++ main.cpp
@@ -1,64 +1,10 @@
 #include <iostream>
-#include "leschoix.hpp"
 #include "leschoix_v2.hpp"
 
 using namespace std;
 
-void testhandler(string opt, vector<string> args){
-    cout << "opt: " << opt << endl;
-
-    cout << "args: ";
-    for (auto x: args){
-        cout << x << " ";
-    }
-    cout << endl;
-}
-
-vector<YY::lesopt> YY::LesChoix::values{
-{'p', "", "8080"},
-{'a', "option", "option"},
-};
-
 int main(int argc, char **argv)
 {
-    auto handle=[](string opt, vector<string> args)->void{
-        if (opt=="port"){
-            cout << "yes it is port " << args.at(0) << endl;
-        }
-    };
-
-//    Y::LesChoix choix2(argc, argv);
-    YY::LesChoix choix(argc, argv);
-
-//    for (auto x: choix.values){
-//        cout << "opt: " << x.opt << " alias: " << x.alias << " values: ";
-//        for (auto j: x.strArgs())
-//            cout << j << " ";
-//        cout << endl;
-//    }
-
-    unsigned port=choix['p'].getArg<uint16_t>();
-    string option=choix["option"].getArg();
-
-    cout << "port: " << port << endl;
-    cout << "option: " << option << endl;
-
-//    cout << choix2["port"].get<uint32_t>();
-
-//    cout << typeid(uint8_t).name() << "\n";
-//    cout << typeid(uint16_t).name() << "\n";
-//    cout << typeid(uint32_t).name() << "\n";
-//    cout << typeid(uint64_t).name() << "\n";
-//    cout << typeid(unsigned long long).name() << "\n";
-//    cout << typeid(int8_t).name() << "\n";
-//    cout << typeid(int16_t).name() << "\n";
-//    cout << typeid(int32_t).name() << "\n";
-//    cout << typeid(long).name() << "\n";
-//    cout << typeid(long long).name() << "\n";
-//    cout << typeid(double).name() << "\n";
-//    cout << typeid(long double).name() << "\n";
-//    cout << typeid(float).name() << "\n";
-//    cout << typeid(char).name() << "\n";
 
     return 0;
 }
