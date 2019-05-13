cede2482bb3ebee25f019c474f8cbbff0ebcd68b
ac1dloop <rakivvode@gmail.com>
2019-05-13 19:09:52 +0300

    adding support for multiple arguments

--- leschoix.hpp
+++ leschoix.hpp
@@ -12,19 +12,30 @@ namespace Y {
 
 using namespace std;
 
+struct Not_found: std::exception {
+    virtual const char * const what() {
+        return "Fuck you";
+    }
+};
+
 struct lesopt {
 
-    lesopt(const string& name):name(name){}
+    lesopt(const string& name):name(name),value({""}){}
 
-    lesopt(const string& name, const string& value):name(name),value(value){}
+    lesopt(const string& name, const string& value):name(name),value({value}){}
 
     template<typename T>
     T Get(){
         return T{};
     }
 
+    template<typename T>
+    vector<T> GetArr(){
+        return vector<T>{};
+    }
+
     string name;
-    string value;
+    vector<string> value;
 };
 
 /* Добавить алиасы */
@@ -33,49 +44,138 @@ struct lesopt {
 template<>
 string lesopt::Get()
 {
-    return value;
+    return value.back();
 }
 
 template<>
 int lesopt::Get()
 {
-    return stoi(value);
+    return stoi(value.back());
 }
 
 template<>
 uint16_t lesopt::Get()
 {
-    return static_cast<uint16_t>(stol(value));
+    return static_cast<uint16_t>(stol(value.back()));
 }
 
 template<>
 double lesopt::Get()
 {
-    return stod(value);
+    return stod(value.back());
 }
 
 template<>
 long double lesopt::Get()
 {
-    return stold(value);
+    return stold(value.back());
 }
 
 template<>
-uint32_t lesopt::Get()
+unsigned long int lesopt::Get()
 {
-    return static_cast<uint32_t>(stoul(value));
+    return static_cast<uint32_t>(stoul(value.back()));
 }
 
 template<>
 unsigned long long int lesopt::Get()
 {
-    return stoull(value);
+    return stoull(value.back());
+}
+
+template<>
+float lesopt::Get()
+{
+    return stof(value.back());
+}
+
+template<>
+vector<string> lesopt::GetArr()
+{
+    return value;
+}
+
+template<>
+vector<int> lesopt::GetArr()
+{
+    vector<int> res;
+
+    for (auto x: value)
+        res.push_back(stoi(x));
+
+    return res;
+}
+
+template<>
+vector<uint16_t> lesopt::GetArr()
+{
+    vector<uint16_t> res;
+
+    for (auto x: value)
+        res.push_back(stol(x));
+
+    return res;
+}
+
+template<>
+vector<double> lesopt::GetArr()
+{
+    vector<double> res;
+
+    for (auto x: value)
+        res.push_back(stod(x));
+
+    return res;
+}
+
+template<>
+vector<long double> lesopt::GetArr()
+{
+    vector<long double> res;
+
+    for (auto x: value)
+        res.push_back(stold(x));
+
+    return res;
+}
+
+template<>
+vector<unsigned long int> lesopt::GetArr()
+{
+    vector<unsigned long int> res;
+
+    for (auto x: value)
+        res.push_back(stoul(x));
+
+    return res;
+}
+
+template<>
+vector<unsigned long long int> lesopt::GetArr()
+{
+    vector<unsigned long long int> res;
+
+    for (auto x: value)
+        res.push_back(stoull(x));
+
+    return res;
+}
+
+template<>
+vector<float> lesopt::GetArr()
+{
+    vector<float> res;
+
+    for (auto x: value)
+        res.push_back(stof(x));
+
+    return res;
 }
 
 struct LesChoix {
 
     LesChoix(int argc, char **argv){
-        Parse(argc, argv);
+        Parse2(argc, argv);
     }
 
     lesopt& operator[](const string& name){
@@ -86,9 +186,7 @@ struct LesChoix {
         if (it!=options.end())
             return *it;
 
-        options.push_back({name});
-
-        return options.at(options.size()-1);
+        throw Not_found();
     }
 
     lesopt& operator[](const char short_name){
@@ -100,9 +198,20 @@ struct LesChoix {
         if (it!=options.end())
             return *it;
 
-        options.push_back({name});
+        throw Not_found();
+    }
 
-        return options.at(options.size()-1);
+    bool Exist(const string& name){
+        return std::find_if(options.begin(), options.end(), [&name](lesopt& op){
+            return op.name==name;
+        })!=options.end();
+    }
+
+    bool Exist(const char short_name){
+        string name=string(1, short_name);
+        return std::find_if(options.begin(), options.end(), [&name](lesopt& op){
+            return op.name==name;
+        })!=options.end();
     }
 
 private:
@@ -145,6 +254,48 @@ private:
 			value=string(argv[i], strlen(argv[i]));
 
             options.push_back({name, value});
+        }
+    }
+
+    void Parse2(int argc, char **argv){
+        string name;
+        string value;
+        string line;
+        for (int i=1;i<argc;++i){
+
+//            cout << "i: " << i << "  ";
+
+            line=string(argv[i], strlen(argv[i]));
+
+//            cout << line << "\n";
+
+            //if it is an option
+            if (line.at(0)=='-'){
+                if (line.at(1)=='-'){
+                    //long option
+//                    cout << "long option: " << name << "\n";
+                    name=line.substr(2, line.size()-1);
+               } else {
+                    //short option
+                    name=string(1, line.at(1));
+//                    cout << "short option: " << name << "\n";
+                }
+
+                size_t j=line.find('=');
+
+                if (j!=string::npos){
+//                    cout << "found '=' !\n";
+                    value=line.substr(j+1, line.size()-j-1);
+                    name=line.substr(line.rfind('-', j)+1, j-line.rfind('-', j)-1);
+//                    cout << "name: " << name << " value: " << value << "\n";
+                    options.push_back({name, value});
+                    continue;
+                } else continue;
+            }
+
+            value=string(argv[i], strlen(argv[i]));
+
+            options.back().value.push_back(value);
         }
     }
 
--- main.cpp
+++ main.cpp
@@ -7,14 +7,23 @@ int main(int argc, char **argv)
 {
     Y::LesChoix parser(argc, argv);
 
-    uint16_t port;
-    string user;
+    uint16_t port=0;
+    string user="";
+    vector<int> vec{0};
 
-    port=parser['p'].Get<uint16_t>();
-    user=parser["username"].Get<string>();
+    if (parser.Exist('p'))
+        port=parser['p'].Get<uint16_t>();
+    if (parser.Exist("username"))
+        user=parser["username"].Get<string>();
+    if (parser.Exist("values"))
+        vec=parser["values"].GetArr<int>();
 
     cout << "port: " << port << endl;
     cout << "user: " << user << endl;
+    cout << "array: ";
+    for (auto x: vec)
+        cout << x << " ";
+    cout << endl;
 
 	return 0;
 }
